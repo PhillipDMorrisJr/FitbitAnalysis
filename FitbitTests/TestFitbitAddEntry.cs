465d161f93cc9d5feac4146bbf896c17e624715f
Phillip Morris <pmorris4@my.westga.edu>
2017-10-19 07:45:45 -0400

    Started unit testing

---
+++ FitbitTests/TestFitbitAddEntry.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FitbitAnalysis_Phillip_Morris.Model;
+
+namespace FitbitTests
+{
+    [TestClass]
+    public class WhenTestingAddFitbitEntryToJournal
+    {
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentException))]
+        public void WhenAddingNullToJournal()
+        { 
+            FitbitJournal fitbitJournal = new FitbitJournal();
+            fitbitJournal.AddEntry(null);
+        }
+
+
+
+    }
+}
