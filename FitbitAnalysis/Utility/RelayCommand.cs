d62d07d2cfa5ee1da95e683aca80d170249ef8f4
William Leatherman <wleathe1@my.westga.edu>
2017-11-06 03:32:20 -0500

    Created RelayCommand class.

---
+++ FitbitAnalysis/Utility/RelayCommand.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace FitbitAnalysis_Phillip_Morris.Utility
+{
+    public class RelayCommand : ICommand
+    {
+        private Action<object> execute;
+        private Predicate<object> canExecute;
+
+        public bool CanExecute(object parameter)
+        {
+            bool result = canExecute == null ? true : canExecute(parameter);
+            return result;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (CanExecute(parameter))
+            {
+                execute(parameter);
+            }
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        /// <summary>
+        /// Typically, protected but made public, so can trigger a manual refresh on the result of CanExecute.
+        /// </summary>
+        public virtual void OnCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
